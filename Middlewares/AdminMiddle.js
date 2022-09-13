4b73a3ce5921c31dfa2ff0371bbec621093dd264
aflah <aflahbacker2000.com>
2022-09-13 12:41:17 +0530

    first commit

---
+++ Middlewares/AdminMiddle.js
@@ -0,0 +1,31 @@
+const jwt = require("jsonwebtoken");
+const Admin = require("../Models/AdminModel");
+
+module.exports.checkAdmin = (req, res, next) => {
+  const token = req.cookies.jwt;
+  if (token) {
+    jwt.verify(token, "nabeel incubation", async (err, decodedToken) => {
+      console.log(34678956789);
+      console.log(err);
+      if (err) {
+        res.json({ status: false });
+        console.log('no token verify');
+        console.log(err);
+        next();
+      } else {
+        const admin = await Admin.findById(decodedToken.id);
+        if (admin) {
+          res.json({ status: true, admin: admin.email });
+        } else {
+          res.json({ status: false });
+          console.log('no admin');
+          next();
+        }
+      }
+    });
+  } else {
+    res.json({ status: false });
+    console.log('no admin token');
+    next();
+  }
+};
